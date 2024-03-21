3b5b76cb605653f17506462997ff84b57f344679
Sarah Shader <sarahshader@gmail.com>
2024-03-21 13:28:17 -0400

    set env

--- fetchDeploymentUrlAndAdminKey.mjs
+++ fetchDeploymentUrlAndAdminKey.mjs
@@ -11,8 +11,5 @@ const response = await fetch("https://api.convex.dev/api/deployment/authorize_pr
         }
     })
 })
-const rawBody = await response.text()
-console.log(rawBody)
-console.log(process.env.CONVEX_PREVIEW_DEPLOY_KEY, process.env.GITHUB_HEAD_REF)
-const body = JSON.parse(rawBody)
+const body = await response.json()
 console.log(`convex_url=${body.url} convex_admin_key=${body.adminKey}`)
